27984b5e72edb5e1bbffbd588f793081216a493e
Patrick Huston <patrickhuston1@gmail.com>
2015-11-03 12:30:22 -0500

    Implements basic servo control

--- arduino/servoControl/servoControl/servoControl.ino
+++ arduino/servoControl/servoControl/servoControl.ino
@@ -6,6 +6,8 @@ int pinAm2 = 5;
 int pinBm2 = 6;
 int pinEnm2 = 7;
 
+int moveM;
+
 
 void setup() {
   Serial.begin(9600);
@@ -16,24 +18,34 @@ void setup() {
   pinMode(pinAm2, OUTPUT);
   pinMode(pinBm2, OUTPUT);
   pinMode(pinEnm2, OUTPUT);
+  
+  digitalWrite(pinEnm1, HIGH);
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    moveM = Serial.parseInt();
+  }  
   
-  pulse(800, 1, 0);
-  delay(2000);
+  if (moveM == 1){
+    pulse(100, 1, 0);
+  }
+  
+  if (moveM == 2){
+    pulse(100, 0, 0);
+  }
 }
 
 void pulse(int steps, int direction, int speed) {
   digitalWrite(pinAm1, direction);
   if (speed == 0) {
     digitalWrite(pinEnm1, LOW);
-    delay(100);
+    delay(15);
     digitalWrite(pinEnm1, HIGH);
   }
   for (int i = 0; i < steps; i=i+1) {
     digitalWrite(pinBm1, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(100);
     digitalWrite(pinBm1, LOW);
   }
 }
